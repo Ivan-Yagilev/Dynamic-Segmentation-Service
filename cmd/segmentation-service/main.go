d2006f077deb0e6a281e80b17bddf0c260bf50ae
Ivan-Yagilev <ivanrosk@mail.ru>
2023-08-24 17:07:00 +0300

    add structure of project

---
+++ cmd/segmentation-service/main.go
@@ -0,0 +1,13 @@
+package main
+
+func main() {
+	//TODO: init config - viper
+
+	//TODO: init logger - logrus
+
+	//TODO: init db - postgres in docker
+
+	//TODO: init router - gin
+
+	//TODO: run server
+}
