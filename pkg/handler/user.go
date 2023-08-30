205d103ad390a56677bf0ce0ed2450a7ff632ffd
Ivan-Yagilev <ivanrosk@mail.ru>
2023-08-30 12:14:48 +0300

    add swag file users

--- pkg/handler/user.go
+++ pkg/handler/user.go
@@ -12,6 +12,17 @@ type getAllUsersResponse struct {
 	Data []segmentation_service.UserResponse `json:"data"`
 }
 
+// @Summary Get All Users
+// @Tags Users
+// @Description get all users
+// @ID get-all-users
+// @Accept  json
+// @Produce  json
+// @Success 200 {object} getAllUsersResponse
+// @Failure 400,404 {object} errorResponse
+// @Failure 500 {object} errorResponse
+// @Failure default {object} errorResponse
+// @Router /user/ [get]
 func (h *Handler) getAllUsers(c *gin.Context) {
 	lists, err := h.services.User.GetAllUsers()
 	if err != nil {
@@ -24,6 +35,18 @@ func (h *Handler) getAllUsers(c *gin.Context) {
 	})
 }
 
+// @Summary Create user
+// @Tags Users
+// @Description create user
+// @ID create-user
+// @Accept  json
+// @Produce  json
+// @Param input body segmentation_service.User true "user info"
+// @Success 200 {integer} integer 1
+// @Failure 400,404 {object} errorResponse
+// @Failure 500 {object} errorResponse
+// @Failure default {object} errorResponse
+// @Router /user/ [post]
 func (h *Handler) createUser(c *gin.Context) {
 	var input segmentation_service.User
 
@@ -42,6 +65,18 @@ func (h *Handler) createUser(c *gin.Context) {
 	})
 }
 
+// @Summary Delete user
+// @Tags Users
+// @Description delete user by id
+// @ID delete-user
+// @Accept  json
+// @Produce  json
+// @Param id path int true "User ID" Format(int64)
+// @Success 200 {object} statusResponse
+// @Failure 400,404 {object} errorResponse
+// @Failure 500 {object} errorResponse
+// @Failure default {object} errorResponse
+// @Router /user/{id} [delete]
 func (h *Handler) deleteUser(c *gin.Context) {
 	id, err := strconv.Atoi(c.Param("id"))
 	if err != nil {
